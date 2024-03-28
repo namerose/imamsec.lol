380b7483274d48da9739af4de568d05e31d027f1
Imamsec <sipalingencore@gmail.com>
2024-03-28 18:46:05 -0500

    Update ImgProfileImage.tsx

--- src/components/atoms/Image/ImgProfileImage.tsx
+++ src/components/atoms/Image/ImgProfileImage.tsx
@@ -5,7 +5,7 @@ type Image = {
 
 export default function ImgProfileImage() {
   const image: Image = {
-    src: "https://cdn.discordapp.com/attachments/993067467914096663/993067797141790780/W7stl0Bq_a-removebg-preview.png",
+    src: "https://media.discordapp.net/attachments/993067467914096663/993067797141790780/W7stl0Bq_a-removebg-preview.png?ex=66125675&is=65ffe175&hm=75a127a262eef6dea181b037a7656fec08d648895e97668cc608185d1f5e47a4&=&format=webp&quality=lossless",
     alt: "Profile picture",
   };
 
